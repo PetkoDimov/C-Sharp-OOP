4ed5a6760d31e8f1a16605c3fd7d378b7f0d2b7c
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 23:16:37 +0300

    Add files via upload

---
+++ Stack of Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
---
+++ Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        private Stack<string> stack;
+        public StackOfStrings()
+        {
+            stack = new Stack<string>();
+        }
+
+        public bool IsEmpty()
+        {
+
+            return this.Count == 0;
+        
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        
+        }
+
+        public int Capacity { get; set; }
+
+    }
+}
