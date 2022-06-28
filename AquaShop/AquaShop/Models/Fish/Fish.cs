c66f9b5df55418c617a1a2bc4df28376cfd07fd6
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 23:08:40 +0300

    Add files via upload

---
+++ AquaShop/AquaShop/Models/Fish/Fish.cs
@@ -0,0 +1,81 @@
+﻿using AquaShop.Models.Fish.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Fish
+{
+    public abstract class Fish : IFish
+    {
+        private List<string> namesOfFishes = new List<string>();
+        private string name;
+        private string species;
+        private int size;
+        private decimal price;
+
+        protected Fish(string name, string species, decimal price)
+        {
+            Name = name;
+            Species = species;
+            Price = price;
+        }
+
+        public string Name 
+        { 
+            get => name; 
+            
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidFishName);
+                }
+                else if(!namesOfFishes.Contains(value))
+                {
+                    namesOfFishes.Add(value);
+                    name = value;
+                }
+            }
+        
+        }
+
+        public string Species 
+        {
+
+            get => species;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidFishSpecies);
+                }
+                    species = value;
+                
+            }
+
+        }
+
+        public int Size { get; protected set; }
+
+        public decimal Price 
+        {
+            get => size;
+
+            private set
+            {
+
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidFishPrice);
+                }
+            }
+        }
+
+        public virtual void Eat()
+        {
+            Size++;
+        }
+    }
+}
---
+++ Validation of Data/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+                Person person = new Person(command[0], command[1], int.Parse(command[2]), decimal.Parse(command[3]));
+                persons.Add(person);
+
+            }
+
+            decimal percentge = decimal.Parse(Console.ReadLine());
+
+            foreach (var item in persons)
+            {
+                item.IncreaseSalary(percentge);
+            }
+            foreach (var item in persons)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
