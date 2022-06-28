1d1f19bada9a3f7acd188915e6dac595e6c3bfc2
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 23:12:46 +0300

    Add files via upload

---
+++ Person/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name , int age)
+        {
+
+            Name = name;
+            Age = age;
+
+
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format("Name: {0}, Age: {1}", Name, Age));
+
+            return sb.ToString();
+
+        }
+    }
+}
